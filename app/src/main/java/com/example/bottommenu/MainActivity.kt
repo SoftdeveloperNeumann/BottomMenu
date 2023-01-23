d233660687941e4dd0bbd5dbceeb6f0b71d8a7f9
Frank <f.neumann.berlin@googlemail.com>
2023-01-23 11:18:09 +0100

    Listener angebunden und Fragmente Designd

--- app/src/main/java/com/example/bottommenu/MainActivity.kt
+++ app/src/main/java/com/example/bottommenu/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.bottommenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.bottommenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
+    lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        
+        setContentView(binding.root)
+        
+        binding.bottomBar.setOnItemSelectedListener { item: MenuItem ->
+            when(item.itemId){
+                R.id.action_home -> Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show()
+
+            }
+            
+            true
+        }
     }
 }
